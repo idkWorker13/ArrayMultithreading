b38e612e131ada9e360fdb21db5b980cd69f6c5d
idkWorker13 <stefanblum2004@gmail.com>
2020-11-21 15:41:59 +0100

    Added Behavoir when there are more cores than Actions

--- src/com/github/idkWorker13/ArrayMultithreading/AM.java
+++ src/com/github/idkWorker13/ArrayMultithreading/AM.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public abstract class AM implements MultithreadingAction{
+public abstract class AM implements MultithreadingAction {
 	
 	private int i_lenght;
 	private ExecutorService executorService;
@@ -22,7 +22,7 @@ public abstract class AM implements MultithreadingAction{
 		this.i_lenght = i_lenght;	
 		this.processorCount = Runtime.getRuntime().availableProcessors();
 		this.executorService = Executors.newFixedThreadPool(processorCount); // Create a Pool equal to the number of processors we have
-		run();	
+		run();
 	}
 	
 	
@@ -30,31 +30,18 @@ public abstract class AM implements MultithreadingAction{
 	
 	// Creates a Stack of Threads and runs them in threads
 	private void run() {
-		int threadSpectrumWidth = i_lenght / processorCount; // How much work does each thread have to do
-		runnables = new PartRun[processorCount];
 		
-		int currentSpecBeginn = 0;
-		int currentSpecEnd = 0;
-		
-		// Initialises the threads, and start running them on the executor
-		for (int i = 0; i < runnables.length; i++) { 
-			
-			currentSpecEnd = currentSpecBeginn + threadSpectrumWidth;
-			
-			if ((i+1) == runnables.length) { // Ensures that the last thread takes everything from the array
-				currentSpecEnd = i_lenght;
-			}
-			
-			runnables[i] = new PartRun(this, currentSpecBeginn, currentSpecEnd);
-			executorService.execute(runnables[i]);
-			
-			currentSpecBeginn = currentSpecBeginn + threadSpectrumWidth;
+		if (i_lenght <= processorCount) { // We have a processor for each job (MultithreadingAction) 
+			initLessAction(i_lenght);
+		} else { // Multiple Jobs (MultithreadingAction) per thread
+			initPlusActions();
 		}
 		
 		
 		
-		executorService.shutdown();
 		
+		// Shutdown Executot
+		executorService.shutdown();
 		try {
 			executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
 		} catch (Exception e) {
@@ -62,6 +49,44 @@ public abstract class AM implements MultithreadingAction{
 		}
 		
 		
+	}
+	
+	// Used when there are multiple jobs for each thread
+	private void initPlusActions() {
+		// start our threads and calculate variables that determine the 
+		runnables = new PartRun[processorCount];
+		int threadSpectrumWidth = i_lenght / processorCount; // How much work does each thread have to do
+		int currentSpecBeginn = 0;
+		int currentSpecEnd = 0;
+		
+		// Initialises the threads, and start running them on the executor
+		for (int i = 0; i < runnables.length; i++) { 
+			currentSpecEnd = currentSpecBeginn + threadSpectrumWidth;
+					
+			if (currentSpecEnd >= i_lenght) { // Ensures that no thread goes over the limit
+				currentSpecEnd = i_lenght;
+			}
+			
+			if ((i+1)==runnables.length) { // Ensures that the last thread does all remaining work
+				currentSpecEnd = i_lenght;
+			}
+					
+			runnables[i] = new PartRun(this, currentSpecBeginn, currentSpecEnd);
+			executorService.execute(runnables[i]);
+					
+			currentSpecBeginn = currentSpecBeginn + threadSpectrumWidth;
+		}
+	}
+	
+	// Used when each Action gets its own Thread
+	private void initLessAction(int nActions) {
+		System.out.println("started initLessAction");
+		runnables = new PartRun[nActions]; // Create a runnable for each Action (i_lenght)
+		
+		for (int i = 0; i < nActions; i++) {
+			runnables[i] = new PartRun(this, i, (i+1)); // Creta a Part run, thats only has one job
+			executorService.execute(runnables[i]);
+		}
 	}
 	
 	
